412a9ea45b5f84f6f8f429c342e75542509b8cb4
tsuru <missigno@gmail.com>
2014-07-09 17:16:24 +0200

    Legge JSON e Orario

--- app/src/main/java/com/dif/orarioauledmi/Orario.java
+++ app/src/main/java/com/dif/orarioauledmi/Orario.java
@@ -4,23 +4,60 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.dif.orarioauledmi.R;
 
-public class Orario extends Activity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Calendar;
+
+import static com.dif.orarioauledmi.Parser.getJSONFromHttpPost;
+
+public class Orario extends Activity {
+    TextView aula;
+    int idlezione[] = {R.id.lezione1, R.id.lezione2, R.id.lezione3, R.id.lezione4, R.id.lezione5, R.id.lezione6, R.id.lezione7, R.id.lezione8, R.id.lezione9, R.id.lezione10, R.id.lezione11};
+    int idora[] = {R.id.ora1, R.id.ora2, R.id.ora3, R.id.ora4, R.id.ora5, R.id.ora6, R.id.ora7, R.id.ora8, R.id.ora9, R.id.ora10, R.id.ora11};
+    int idprof[] = {R.id.prof1, R.id.prof2, R.id.prof3, R.id.prof4, R.id.prof5, R.id.prof6, R.id.prof7, R.id.prof8, R.id.prof9, R.id.prof10, R.id.prof11};
+    int idRelative[] = {R.id.primo,R.id.secondo,R.id.terzo,R.id.quarto,R.id.quinto,R.id.sesto,R.id.settimo,R.id.ottavo,R.id.nono,R.id.decimo,R.id.undicesimo};
+    String orari[] = {"9-10","10-11","11-12","12-13","14-15","15-16","16-17","17-18"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //Forza la portrait mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //Forza la portrait mode
         setContentView(R.layout.activity_orario);
+        Calendar rightNow = Calendar.getInstance();
+        int f = rightNow.get(Calendar.HOUR_OF_DAY) - 10;
+        aula = (TextView) findViewById(R.id.aula);
         Intent intent = getIntent();
         String d =intent.getStringExtra(QrScan.QRCODE);
-        TextView aula = (TextView) findViewById(R.id.aula);
-        aula.setText(d);
+
+        JSONObject json = getJSONFromHttpPost(d);
+        try {
+            int a = 0;
+            aula.setText(""+json.get("aula"));
+            for(int i = f; i<8;i++){
+                TextView orario =(TextView) findViewById(idora[a]);
+                orario.setText(""+orari[i]);
+                TextView lezione = (TextView) findViewById(idlezione[a]);
+                lezione.setText(""+json.get(orari[i]));
+                a++;
+            }
+            for(int b = a; b < 11; b++){
+                RelativeLayout riga = (RelativeLayout) findViewById(idRelative[b]);
+                riga.setVisibility(View.INVISIBLE);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
--- app/src/main/java/com/dif/orarioauledmi/Parser.java
+++ app/src/main/java/com/dif/orarioauledmi/Parser.java
@@ -1,36 +1,85 @@
 package com.dif.orarioauledmi;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.dif.orarioauledmi.R;
+import android.os.StrictMode;
 
-public class Parser extends Activity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parser);
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by franz on 09/07/14.
+ */
+public class Parser {
+
+    public static JSONObject getJSONFromHttpPost(String URL) {
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            // Create a new HttpClient and Post Header
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(URL);
+            String resultString = null;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.parser, menu);
-        return true;
-    }
+            long t = System.currentTimeMillis();
+            HttpResponse response = (HttpResponse) httpclient.execute(httpPost);
+            System.out.println("HTTPResponse received in [" + (System.currentTimeMillis() - t) + "ms]");
+            // Get hold of the response entity (-> the data):
+            HttpEntity entity = response.getEntity();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            if (entity != null) {
+                // Read the content stream
+                InputStream instream = entity.getContent();
+
+                // convert content stream to a String
+                resultString = convertStreamToString(instream);
+                instream.close();
+                System.out.println("result String : " + resultString);
+                //resultString = resultString.substring(1,resultString.length()-1); // remove wrapping "[" and "]"
+                System.out.println("result String : " + resultString);
+                // Transform the String into a JSONObject
+                JSONObject jsonObjRecv = new JSONObject(resultString);
+                // Raw DEBUG output of our received JSON object:
+                System.out.println("<JSONObject>\n" + jsonObjRecv.toString() + "\n</JSONObject>");
+
+
+                return jsonObjRecv;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+        return null;
+
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line ="";
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
